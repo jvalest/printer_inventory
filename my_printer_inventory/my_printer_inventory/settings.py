baf48d560641df72e62f51b1a69e6d88566fe4a1
jvalest <jvalest@idevops.io>
2024-06-20 15:43:20 -0700

    fixed connection issues with docker containers

--- my_printer_inventory/my_printer_inventory/settings.py
+++ my_printer_inventory/my_printer_inventory/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=$&6!m9#g50fkoz_191_^*jmq6c&+26t^y!w_f2ri3je3ugdo9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
 
 
 # Application definition
