82bedf39eb6821fc3cf9a3a54cc5714f60beb26c
jvalest <jvalest@idevops.io>
2024-06-20 15:29:06 -0700

    updated gitignore

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,24 @@
+# Use an official Python runtime as a parent image
 FROM python:3.9
+
+# Set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
-WORKDIR /app
-COPY requirements.txt /app/requirements.txt
+
+# Set the working directory in the container
+WORKDIR /code
+
+# Copy the dependencies file to the working directory
+COPY requirements.txt /code/
+
+# Install dependencies
 RUN pip install -r requirements.txt
+
+# Copy the current directory contents into the container at /code/
 COPY . /app/
+
+# Expose the port the app runs on
+EXPOSE 8000
+
+# Run the Django app
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- my_printer_inventory/my_printer_inventory/settings.py
+++ my_printer_inventory/my_printer_inventory/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,16 +75,14 @@ WSGI_APPLICATION = 'my_printer_inventory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-from decouple import config
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('MYSQL_DATABASE'),
-        'USER': config('MYSQL_USER'),
-        'PASSWORD': config('MYSQL_PASSWORD'),
-        'HOST': config('DB_HOST', 'db'),  # Use 'db' as default from .env
-        'PORT': config('DB_PORT', '3306'),  # Use '3306' as default from .env
+        'NAME': 'my_printer_inventory_db',    
+        'USER': 'admin',                      
+        'PASSWORD': 'thegoldrush1',           
+        'HOST': 'db',                         
+        'PORT': '3306',                      
     }
 }
 
