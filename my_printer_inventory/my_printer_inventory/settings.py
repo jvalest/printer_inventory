b4b8b31f3b48e5314ea911349e144bb85cc9d26f
jvalest <jvalest@idevops.io>
2024-06-20 14:27:08 -0700

    dockerized app

--- my_printer_inventory/my_printer_inventory/settings.py
+++ my_printer_inventory/my_printer_inventory/settings.py
@@ -74,13 +74,18 @@ WSGI_APPLICATION = 'my_printer_inventory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('MYSQL_DATABASE'),
+        'USER': config('MYSQL_USER'),
+        'PASSWORD': config('MYSQL_PASSWORD'),
+        'HOST': config('DB_HOST', 'db'),  # Use 'db' as default from .env
+        'PORT': config('DB_PORT', '3306'),  # Use '3306' as default from .env
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
